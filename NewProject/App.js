7d97e28e4f0d36589113b996f9b3a8c310cfcd12
Nadiia Maltseva <amanavi2021@gmail.com>
2023-09-24 14:07:55 +0300

    Update App.js

--- NewProject/App.js
+++ NewProject/App.js
@@ -17,8 +17,8 @@ export default function App() {
 
   return (
     <>
-      {/* <RegistrationScreen /> */}
-      <LoginScreen />
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
       <PostsScreen />
     </>
   );
